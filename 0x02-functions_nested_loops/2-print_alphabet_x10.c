46ef1fb17431af7cb48b85d0cf7fdc8ede4ada39
Emmanuel Yeboah <yeboahmanuemmanuel@gmail.com>
2022-09-14 15:53:58 -0700

    c nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print a - z ten times
+ * Reaturn: 0 for success
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; j <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
