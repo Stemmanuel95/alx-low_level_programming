ebfd5a043ee2cee0803326c57a73bc1112358fb0
Emmanuel Yeboah <yeboahmanuemmanuel@gmail.com>
2022-09-15 15:33:09 -0700

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - test that the function prints negative or positive integer
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 20;
+	positive_or_negative(i);
+
+	return (0);
+
+
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+Debugging some cool c programs
