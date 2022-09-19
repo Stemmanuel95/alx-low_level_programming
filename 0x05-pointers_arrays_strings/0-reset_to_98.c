ab7ccca8073c7501b94adfb1db22e3761e4e3f9d
Emmanuel Yeboah <yeboahmanuemmanuel@gmail.com>
2022-09-19 16:06:49 -0700

    C programing getting realistic in practice

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets value of pointer to 98
+ * @n: pointer to reset to 98
+ *
+ * Return: void
+ *
+ * Code written by EMmanuel Yeboah
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+C programming getting more realistic
