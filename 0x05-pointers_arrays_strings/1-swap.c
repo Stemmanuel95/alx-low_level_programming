fe1d8ffc66b9a9806925a0d4c1208952e5d0c366
Emmanuel Yeboah <yeboahmanuemmanuel@gmail.com>
2022-09-19 16:17:28 -0700

    C programing getting realistic in practice

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of integer a and integer b
+ * @i: first integer to swap
+ * @j: second integer to swap
+ *
+ * Return: void
+ *
+ * Code written by EMmanuel Yeboah
+ */
+
+void swap_int(int *i, int *j)
+{
+	int k;
+
+	k = *i;
+	*i = *j;
+	*j = k;
+
+
+}
