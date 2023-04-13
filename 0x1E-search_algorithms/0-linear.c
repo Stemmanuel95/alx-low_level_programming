42b27d250b068a24f2535b446a0edb003ee958b3
Emmanuel Yeboah Manu <yeboahmanuemmanuel@gmail.com>
2023-04-13 23:43:51 +0000

    Function searches for a value in a list of interfers using a linear search algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,25 +1,25 @@
 #include "search_algos.h"
 
 /**
- * linear_search - This variable searches for a value in the array of
- * integers using a linear search algorithm
+ * linear_search - variable searches for a value in an array of 
+ * integers using the Linear search algorithm
  *
  * @array: input array
  * @size: size of the array
  * @value: value to search in
- * Return: Always Exit_Success
+ * Return: Always EXIT_SUCCESS
  */
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t x;
+	size_t x = 0;
 
 	if (array == NULL)
 	{
 		return (-1);
 	}
 
-	for (x = 0; x < size; x++)
+	for (; x < size; x++)
 	{
 		printf("Value checked array[%li] = [%i]\n", x, array[x]);
 		if (array[x] == value)
